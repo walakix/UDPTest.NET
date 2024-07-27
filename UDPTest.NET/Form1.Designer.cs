c701a868758da44e253dce1cddea4b5fc386af78
walakix <gwalaki@gmail.com>
2024-07-27 09:46:16 +0200

    First

---
+++ UDPTest.NET/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace UDPTest.NET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numServerPort = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.tbDestIP = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numDestPort = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.numSrcPort = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numServerPort)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDestPort)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSrcPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numServerPort);
+            this.groupBox1.Location = new System.Drawing.Point(3, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 59);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.numSrcPort);
+            this.groupBox2.Controls.Add(this.btnSend);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.tbMessage);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.numDestPort);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbDestIP);
+            this.groupBox2.Location = new System.Drawing.Point(3, 74);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(791, 59);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Client";
+            // 
+            // numServerPort
+            // 
+            this.numServerPort.Location = new System.Drawing.Point(6, 30);
+            this.numServerPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numServerPort.Name = "numServerPort";
+            this.numServerPort.Size = new System.Drawing.Size(81, 20);
+            this.numServerPort.TabIndex = 0;
+            this.numServerPort.Value = new decimal(new int[] {
+            9910,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port:";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(93, 27);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(174, 27);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lbLog
+            // 
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.Location = new System.Drawing.Point(3, 142);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(791, 303);
+            this.lbLog.TabIndex = 4;
+            // 
+            // tbDestIP
+            // 
+            this.tbDestIP.Location = new System.Drawing.Point(8, 29);
+            this.tbDestIP.Name = "tbDestIP";
+            this.tbDestIP.Size = new System.Drawing.Size(137, 20);
+            this.tbDestIP.TabIndex = 0;
+            this.tbDestIP.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DestIP:";
+            // 
+            // numDestPort
+            // 
+            this.numDestPort.Location = new System.Drawing.Point(151, 30);
+            this.numDestPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numDestPort.Name = "numDestPort";
+            this.numDestPort.Size = new System.Drawing.Size(66, 20);
+            this.numDestPort.TabIndex = 2;
+            this.numDestPort.Value = new decimal(new int[] {
+            9910,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(148, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "DestPort:";
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.Location = new System.Drawing.Point(310, 29);
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(219, 20);
+            this.tbMessage.TabIndex = 4;
+            this.tbMessage.Text = "Hello!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(307, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Message:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(535, 27);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(702, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // numSrcPort
+            // 
+            this.numSrcPort.Location = new System.Drawing.Point(223, 30);
+            this.numSrcPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numSrcPort.Name = "numSrcPort";
+            this.numSrcPort.Size = new System.Drawing.Size(81, 20);
+            this.numSrcPort.TabIndex = 7;
+            this.numSrcPort.Value = new decimal(new int[] {
+            9900,
+            0,
+            0,
+            0});
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(220, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "SrcPort:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "UDPTest.NET";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numServerPort)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numDestPort)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSrcPort)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numServerPort;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbDestIP;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numDestPort;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numSrcPort;
+    }
+}
+
---
+++ UDPTest.NET/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Data.Common;
+using System.Threading;
+
+
+namespace UDPTest.NET
+{
+    public partial class Form1 : Form
+    {
+        Thread thdUDPServer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            UDPServerStart();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            UDPServerStop();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            UDPSend("-");
+        }
+
+
+        public void UDPServerThread()
+        {
+            UdpClient udpClient = new UdpClient((int)numServerPort.Value);
+            while (true)
+            {
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+                WriteLog(RemoteIpEndPoint.Address.ToString()
+                                        + ":" + returnData.ToString());
+            }
+            //udpClient.Close();
+        }
+
+        private void WriteLog(string msg)
+        {
+            lbLog.Invoke((MethodInvoker)(() => lbLog.Items.Add(msg)));
+        }
+
+        private void UDPServerStart()
+        {
+            thdUDPServer = new Thread(new ThreadStart(UDPServerThread));
+            thdUDPServer.Start();
+            btnStart.Enabled = false; 
+            numServerPort.Enabled = false;
+            btnStop.Enabled=true;
+        }
+
+        private void UDPServerStop()
+        {
+            thdUDPServer.Abort();
+            thdUDPServer=null;
+            btnStart.Enabled = true;
+            numServerPort.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void UDPSend(string msg)
+        {
+            UdpClient sender1 = new UdpClient();
+            sender1.Send(Encoding.UTF8.GetBytes(tbMessage.Text), 3, tbDestIP.Text, (int)numDestPort.Value);
+            sender1.Close();
+        }
+    }
+}
